c6430ae338d6298a4a3eba35ea30ebbb35625356
Aditya Grover <aditya@Adityas-MacBook-Pro.local>
2015-07-02 22:48:57 +0530

    BIF to MLN converter added in utils

---
+++ utils/bif_to_mln.py
@@ -0,0 +1,153 @@
+
+# Converts a BIF (Bayesian Interchange Format) File to an MLN (Markov Logic Network) File
+
+
+import sys, re, math
+
+def parseBIF():
+
+    # portions of parseBIF taken from https://github.com/eBay/bayesian-belief-networks/blob/master/bayesian/examples/bif/bif_parser.py
+
+    # Regex patterns for parsing
+    infile = open(sys.argv[1], 'r')
+    variable_pattern = re.compile(r'  type discrete \[ \d+ \] \{ (.+) \};\s*')
+    prior_pattern_1 = re.compile(r'probability \( ([^|]+) \) \{\s*')
+    prior_pattern_2 = re.compile(r'  table (.+);\s*')
+    cpt_pattern_1 = re.compile(r'probability \( (.+) \| (.+) \) \{\s*')
+    cpt_pattern_2 = re.compile(r'  \((.+)\) (.+);\s*')
+
+# storage data structures
+    variables = {}
+    cpt = {}
+
+    while True:
+        line = infile.readline()
+
+        # End of BIF file
+        if not line:
+            break
+        # print str(line.startswith('variable'))
+
+        if line.startswith('variable'):
+            match = variable_pattern.match(infile.readline())
+            if match:
+                variables[line[9:-3]] = match.group(1).split(', ')
+            else:
+                raise Exception('Unrecognised variable declaration:\n' + line)
+            infile.readline()
+        elif line.startswith('probability'):
+
+            match = prior_pattern_1.match(line)
+            if match:
+                variable = match.group(1)
+                line = infile.readline()
+                match = prior_pattern_2.match(line)
+                if match:
+                    parent_assignments = []
+                    prob_values = match.group(1).split(', ')
+                    cpt[variable] = (parent_assignments, prob_values)
+                else:
+                    raise Exception('Unrecognised prior probability table entry:\n' + line)
+            else:
+                match = cpt_pattern_1.match(line)
+
+                if match:
+                    variable = match.group(1)
+                    parents = match.group(2).split(', ')
+                    cpt_table = []
+                    line = infile.readline()
+                    while line[0] != '}':                        
+                        # print 'h' + line
+                        match = cpt_pattern_2.match(line)
+                        if match:
+                            parent_values = match.group(1).split(', ')
+                            parent_assignments = zip(parents, parent_values)
+                            prob_values = match.group(2).split(', ')
+                            cpt_table.append((parent_assignments, prob_values))
+                        else:
+                            print line
+                            raise Exception('Unrecognised cpt table entry:\n' + line)
+                        line = infile.readline()
+                    cpt[variable] = cpt_table;
+                else:
+                    raise Exception('Unrecognised probability declaration:\n' + line)
+
+    infile.close()
+    return (variables, cpt)
+
+def addUnary(variables, MLNobject):
+
+    for var in variables:
+        MLNline = var.upper() + '(' + var.lower() + 'Value!)'
+        MLNobject.append(MLNline)
+    MLNobject.append('')
+    return
+    # outobj.writerow('')
+    # outobj.writerow()
+
+def addCPT(parsedBIF, MLNobject):
+    variables = parsedBIF[0]
+    cpt = parsedBIF[1]
+
+    for var, cpt_entry in cpt.iteritems():
+        if not cpt_entry[0]:
+            parent_assignments = cpt_entry[0]
+            prob_values = cpt_entry[1]
+            for (value, prob) in zip(variables[var], prob_values):
+                if (prob != '0.0'):
+                    MLNline = str(-1*math.log(float(prob))) + ' !' + var.upper() + '(' + var.upper() + value.upper() + ')'
+                    MLNobject.append(MLNline)
+        else:
+            for entry in cpt_entry:
+                parent_assignment = entry[0]
+                prob_values = entry[1]
+                for (value, prob) in zip(variables[var], prob_values):
+                    if (prob != '0.0'):
+                        MLNline = str(-1*math.log(float(prob))) + ' !' + var.upper() + '(' + var.upper() + value.upper() + ')'
+                        for (parent, parent_val) in parent_assignment:
+                            MLNline2 = MLNline + ' V !' + parent.upper() + '(' + parent.upper() + parent_val.upper() + ')'
+                            MLNobject.append(MLNline2) 
+
+    return
+    # outobj.writerow('')
+
+def writeMLN(MLNobject):
+    with open(sys.argv[2], 'wb') as outfile:
+        for line in MLNobject:
+            outfile.write(line + '\n')
+    return
+
+def printParsedBIF(parsedBIF):
+    variables = parsedBIF[0]
+    cpt = parsedBIF[1]
+
+    print variables
+    print cpt
+    return
+
+def printMLN(MLNobject):
+    for line in MLNobject:
+        print line
+def main():
+
+    # read BIF file
+    parsedBIF = parseBIF()
+
+    printParsedBIF(parsedBIF)
+
+    MLNobject = []
+
+    # add unary predicates
+    addUnary(parsedBIF[0], MLNobject)
+
+    # add disjunction clauses for CPT entries
+    addCPT(parsedBIF, MLNobject)
+
+    printMLN(MLNobject)
+    # write MLN file
+    writeMLN(MLNobject)
+    return
+
+if __name__ == '__main__':
+    main()
+#EOF
